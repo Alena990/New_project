38817feaf80bd94c5d41057945b9fb2542806612
Alena990 <alenaa.bva@gmail.com>
2021-10-30 01:22:48 +0400

    Добавлен метод расчета calc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,38 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello world");// write your code here
+        System.out.println("Hello world");
+        System.out.println(calc(6, 4, 'k'));
+
     }
-}
+
+    public static double calc(double a, double b, char operator) {
+        switch (operator) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+            default:
+                return 0;
+        }
+    }
+
+//        if (operator == '+') {
+//            return a + b;
+//        }
+//        else if (operator == '-'){
+//            return a - b;
+//        }
+//        else if (operator == '*'){
+//            return a * b;
+//        }
+//        else{
+//            return a / b;
+//        }
+    }
+
+
